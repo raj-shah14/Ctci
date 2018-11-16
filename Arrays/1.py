2cfd462b4de759b26e7d1ba7aca70684154978d7
raj-shah14 <rajpareshshah14@gmail.com>
2018-11-16 08:08:14 -0600

    without Data structure

--- Arrays/1.py
+++ Arrays/1.py
@@ -1,7 +1,9 @@
-string1 = "asdfknerlgn"
+string1 = "asdfknerlg"
 string2 = "m,dbnjgdboceang"
 string3 = ",./';][]./[./"
 
+
+#With Data structures
 def isUnique(string):
     unwanted = ",./;'[]\<}>?:{+*-!@#$%^&()"
 
@@ -15,6 +17,20 @@ def isUnique(string):
             return False
     return True
 
-print(isUnique(string1))
-print(isUnique(string2))
-print(isUnique(string3))
+#Without Data Structures
+def isUnique2(string):
+    if len(string)> 128:  #Assuming ASCII character set of 128
+        return False
+    
+    char_set = [False for _ in range(128)]
+    for i in string:
+        val = ord(i)
+
+        if char_set[val]:
+            return False
+        char_set[val] = True
+    return True
+
+print(isUnique2(string1))
+print(isUnique2(string2))
+print(isUnique2(string3))
