a5548e76fc506148c927317457cf3f7082c6ae9c
raj-shah14 <rajpareshshah14@gmail.com>
2018-12-01 18:44:34 -0600

    Added List Comprehension concatenation

--- Arrays/6.py
+++ Arrays/6.py
@@ -4,9 +4,9 @@
 #str1 = "aaabbbcccAAAfffg"
 #str1 = "ab"
 #str1 = "aabb"
-#str1 = "aabcccccccfjkjklllllzzy"
+str1 = "aabcccccccfjkjklllllzzy"
 #str1 = "aaabbcccccddd"
-str1 = "aaaaa"
+#str1 = "aaaaa"
 
 def compress(str1):
     finstr = ""
@@ -18,15 +18,46 @@ def compress(str1):
             count += 1
         else:
             finstr += prev
-            finstr += str(count)
+            finstr += `count`
+            #finstr += str(count)      #Instead of using str, one should use backticks (``)
             count = 1
             prev = str1[i]
         i = i+1
     finstr += prev
-    finstr += str(count)
+    finstr += `count`      #Converts int to string
+    #finstr += str(count)
 
     if len(str1) <= len(finstr):
         return str1
     return finstr
 
-print(compress(str1))  #O(n) time complexity 
+print(compress(str1))  #O(n^2) time complexity 
+
+#Alternate Method using List Comprehension
+def compress2(str1):
+    finstr = []
+    prev = str1[0]
+    count = 1
+    i = 1
+    while i < len(str1):
+        if(str1[i] == prev):
+            count += 1
+        else:
+            finstr.append(prev)
+            finstr.append(count)
+            count = 1
+            prev = str1[i]
+        i = i+1
+    finstr.append(prev)
+    finstr.append(count)
+    str2 = ''.join([str(num) for num in finstr])
+
+#String Concatenation is a costly operator as it creates a new copy of string and copies old contents
+#A fastest approach would be to use list comprehension.
+
+
+    if len(str1) <= len(str2):
+        return str1
+    return str2
+
+print(compress2(str1))  #O(n^2) time complexity but slightly better.
