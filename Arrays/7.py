597b984f603ecaf0695577380eb8e9dc264219dc
raj-shah14 <rajpareshshah14@gmail.com>
2018-12-02 21:38:26 -0600

    Updated code and alt const space method

--- Arrays/7.py
+++ Arrays/7.py
@@ -35,3 +35,37 @@ for i in range(len(mat2)):
     for j in range(len(mat2[i])):
         print(mat2[i][j]),
     print('')                   #O(n^2) time and space Complexity
+
+
+
+print("Alternate Method Constant Space")
+#Alt Method
+mat1 = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+
+
+def rotate_mat_const(mat1):
+    n = len(mat1)
+    for layer in range(0,int(n/2)):
+        first = layer
+        last = n - 1 - layer
+        for i in range(first,last):
+            offset = i - first
+            top = mat1[first][i]
+            #left -> top
+            mat1[first][i] = mat1[last - offset][first]
+
+            #bottom ->left
+            mat1[last-offset][first] = mat1[last][last-offset]
+
+            #right->bottom
+            mat1[last][last-offset] = mat1[i][last]
+
+            #top->right
+            mat1[i][last] = top
+    return mat1
+
+mat1 = rotate_mat_const(mat1)    
+for i in range(len(mat1)):
+    for j in range(len(mat1[i])):
+        print(mat1[i][j]),
+    print('')               #O(N^2) time complexity but constant Space
