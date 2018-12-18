964cc8f7fbb83f79b53a3a3d13f8c293f02efedc
raj-shah14 <rajpareshshah14@gmail.com>
2018-12-18 16:10:40 -0600

    Updated Classes Code

---
+++ Linkedlist/1.py
@@ -0,0 +1,33 @@
+#Write code to remove duplicates from an unsorted linked list.
+
+from Class_Linkedlist import randomLinkedList
+
+def delDuplicates(linkedlist):  #O(n^2) solution
+    curr = linkedlist.head
+    while curr is not None:
+        runn = curr
+        while runn.next is not None:
+            if runn.next.value == curr.value:
+                runn.next = runn.next.next
+            else:
+                runn = runn.next
+        curr = curr.next
+
+
+# Alternate solution
+
+def delDuplicates2(linkedlist):     #O(n) solution
+    curval = linkedlist.head
+    tempdict = {curval.value : True}
+    while curval.next is not None:
+        if curval.next.value in tempdict:
+            curval.next = curval.next.next
+        else:
+            tempdict[curval.next.value] = True
+            curval = curval.next
+
+
+L1 = randomLinkedList(9, 3, 7)  #Length, min ,max
+print(L1)
+delDuplicates2(L1)
+print(L1)
