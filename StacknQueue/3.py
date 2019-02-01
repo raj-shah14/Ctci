979f6f9845c4eb3d3035fc226c2dc1a7a4ade836
raj-shah14 <rajpareshshah14@gmail.com>
2019-02-01 15:07:41 -0600

    Updated code for Stackofplates and comments

--- StacknQueue/3.py
+++ StacknQueue/3.py
@@ -7,59 +7,99 @@
 # FOLLOW UP
 # Implement a function popAt (int index) which performs a pop operation on a specific sub-stack.
 
-class Stack:
+class StackofPlates:
     def __init__(self,thresh):
-        self.thresh = thresh
-        self.arr = []
+        self.thresh = thresh        #capacity of stack
+        self.setOfStacks = []       #Main stack
     
-    def isFull(self):
-        return len(self.arr) == self.thresh
+    def isFull(self):               #Check if substack is full
+        return len(self.setOfStacks[0]) == self.thresh
+
+    def isEmpty(self):              # check if main stack is empty
+        return self.setOfStacks == []
 
     def push(self,value):
         self.value = value
-        if self.isFull:
-            pass
-            #TODO make new stack and add to that
+        if self.isEmpty():
+            self.setOfStacks.insert(0,[self.value])     # If main stack empty push at first index a list
         else:
-            self.arr.insert(0,self.value)
-
+            if self.isFull():                           # If sub stack at its full capacity make new stack
+                self.setOfStacks.insert(0,[self.value])
+            else:
+                self.setOfStacks[0].insert(0,self.value) # else insert into the existing stack
 
     def pop(self):
-        top = self.arr[0]
-        del self.arr[0]
-        return top
-    
-    def isEmpty(self):
-        return self.arr == []
-
-    def peek(self):
-        return self.arr[0]
-
+        if self.isEmpty():
+            return "Stack is empty"
+        else:
+            if len(self.setOfStacks[0]) == 1:           # if substack has just one element, pop and delete sub stack
+                popval = self.setOfStacks[0][0]
+                del self.setOfStacks[0]
+            else:
+                popval = self.setOfStacks[0][0]         # Else return and delete value
+                del self.setOfStacks[0][0]
+            return popval
+        
     def showStack(self):
-        print(self.arr)
+        print(self.setOfStacks)
+
+    def popAt(self,idx):                #index starts from zero
+        if self.isEmpty():
+            return "Stack is Empty"
+        else:
+            if len(self.setOfStacks[0]) == 1:
+                if idx != 0:
+                    return "index not found"
+                else:
+                    retval = self.setOfStacks[0][idx] 
+                    del self.setOfStacks[0]
+                    return retval
+            else:
+                if len(self.setOfStacks[0]) -1 < idx:
+                    return "Value not found"
+                else:
+                    retval = self.setOfStacks[0][idx] 
+                    del self.setOfStacks[0][idx]
+                    return retval
+
+sop = StackofPlates(5)
+sop.push(5)
+sop.push(6)
+sop.push(7)
+sop.push(8)
+sop.push(9)
+sop.push(1)
+sop.push(2)
+sop.push(3)
+sop.push(4)
+sop.push(10)
+sop.push(11)
+sop.push(12)
+
+sop.showStack()
+print(sop.pop())
+sop.showStack()
+print(sop.pop())
+sop.showStack()
+print(sop.pop())
+sop.showStack()
+print(sop.pop())
+sop.showStack()
+print(sop.pop())
+sop.showStack()
+
+sop.push(52)
+sop.push(33)
+sop.push(51)
+sop.push(50)
+sop.push(37)
 
 
-class SOP:
-    def __init__(self):
-        self.setofstacks = []
+print(sop.popAt(1))
+sop.showStack()
 
-    def push(self,Stack):
-        self.Stack = Stack
-        self.setofstacks.insert(0,self.Stack)
+print(sop.popAt(0))
+sop.showStack()
 
-    def showsop(self):
-        print(self.setofstacks)
-    
-s = Stack(5)
-s.push(5)
-s.push(10)
-s.push(23)
-s.push(24)
-s.push(6)
-
-print(s.isFull())
-s.showStack()
-sop = SOP()
-sop.push(s)
-
-sop.showsop()
+print(sop.popAt(2))
+sop.showStack()
