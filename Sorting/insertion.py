8b053745a48b5ead6cff89ab4f69fb0be1c49733
Raj <raj@Larrys-MacBook-Air.local>
2019-04-13 18:20:14 -0500

    insertion sort code

---
+++ Sorting/insertion.py
@@ -0,0 +1,15 @@
+# Implementation of Insertion sort
+
+arr = [14,22,34,10,32,99,43,76,8,40]
+
+for i in range(1,len(arr)):
+    hole = i
+    val = arr[hole]
+
+    while hole > 0 and arr[hole-1] > val:
+        arr[hole] = arr[hole-1]
+        hole = hole -1 
+
+    arr[hole] = val
+
+print(arr)
