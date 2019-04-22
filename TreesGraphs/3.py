429bd39fa4574e42755fa8286b5b6848691ea1be
raj-shah14 <rajpareshshah14@gmail.com>
2019-04-22 15:51:03 -0700

    code for 3rd

--- TreesGraphs/3.py
+++ TreesGraphs/3.py
@@ -1,2 +1,15 @@
 # List of Depths: Given a binary tree, design an algorithm which creates a linked list of all the nodes
 # at each depth (e.g., if you have a tree with depth D, you'll have D linked lists).
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class LinkedList:
+    def __init__(self,head):
+        self.head = head
+        self.next = None
+
+
+
